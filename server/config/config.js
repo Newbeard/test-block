e36388ce10a6264b19c332ae55e7dbeba0a306b8
Alex <alekscherniaev@gmail.com>
2022-05-18 11:32:38 +0300

    update deploy

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,8 @@ const config = (app) => {
   app.use(express.urlencoded({
     extended: true,
   }));
-  app.use(express.static(path.join(process.env.PWD, 'public')));
+  app.use('/public', express.static(path.join(process.env.PWD, 'public')));
+  app.use('/build', express.static(path.join(process.env.PWD, 'build')));
   app.use(fileUpload());
   app.use(cookieParser());
   app.use(cors(corsConfig));
